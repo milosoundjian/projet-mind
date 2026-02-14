27ee7eed8f4be3a36ae8462b7e7ab4c235fa4c0a
Vladislav Antipin <anvlan1306@icloud.com>
2026-02-14 21:41:58 +0100

    small fixes for collect_uniform_transitions

--- src/pmind/replay.py
+++ src/pmind/replay.py
@@ -57,71 +57,69 @@ class UniformWrapper(Wrapper):
 def format_tensor(item, dtype=torch.float32):
     return torch.tensor(item, dtype=dtype).unsqueeze(0)
 
+# Vlad's version: 
+# def collect_uniform_transitions(env_name: str, buffer_size: int):
 
-def collect_uniform_transitions(env_name: str, buffer_size: int):
+#     # NOTE: expected buffer composition and types:
+#     # env/env_obs : torch.float32
+#     # env/terminated : torch.bool
+#     # env/truncated : torch.bool
+#     # env/done : torch.bool
+#     # env/reward : torch.float32
+#     # env/cumulated_reward : torch.float32
+#     # env/timestep : torch.int64
+#     # action : torch.float32
 
-    # NOTE: expected buffer composition and types:
-    # env/env_obs : torch.float32
-    # env/terminated : torch.bool
-    # env/truncated : torch.bool
-    # env/done : torch.bool
-    # env/reward : torch.float32
-    # env/cumulated_reward : torch.float32
-    # env/timestep : torch.int64
-    # action : torch.float32
+#     env = UniformWrapper(gym.make(env_name))
+#     rb = ReplayBuffer(buffer_size)
+#     for _ in range(buffer_size):
+#         workspace = Workspace()
 
+#         # TODO: what to do in case when we teleport into a terminal state directly?
+#         # it can be bad since uniform exploration becomes filled with garbage transitions
+#         state = env.uniform_reset()
+#         action = env.uniform_action()
+
+#         workspace.set("env/env_obs", 0, format_tensor(state))
+#         workspace.set("action", 0, format_tensor(action))
+#         workspace.set("env/timestep", 0, format_tensor(0, torch.int64))
+
+#         observation, reward, terminated, truncated, _ = env.step(action)
+
+#         workspace.set("env/env_obs", 1, format_tensor(observation))
+#         workspace.set("env/terminated", 1, format_tensor(terminated, torch.bool))
+#         workspace.set("env/truncated", 1, format_tensor(truncated, torch.bool))
+#         workspace.set("env/done", 1, format_tensor(terminated or truncated, torch.bool))
+#         workspace.set("env/reward", 1, format_tensor(reward))
+#         workspace.set("env/cumulated_reward", 1, format_tensor(reward))
+#         next_action = env.action_space.sample()
+#         workspace.set("action", 1, format_tensor(next_action))
+#         workspace.set("env/timestep", 1, format_tensor(1, torch.int64))
+
+#         rb.put(workspace.get_transitions())
+#     return rb
+
+
+def collect_uniform_transitions(env_name: str, buffer_size: int = 100_000):
     env = UniformWrapper(gym.make(env_name))
-    rb = ReplayBuffer(buffer_size)
-    for _ in range(buffer_size):
-        workspace = Workspace()
-
-        # TODO: what to do in case when we teleport into a terminal state directly?
-        # it can be bad since uniform exploration becomes filled with garbage transitions
-        state = env.uniform_reset()
-        action = env.uniform_action()
-
-        workspace.set("env/env_obs", 0, format_tensor(state))
-        workspace.set("action", 0, format_tensor(action))
-        workspace.set("env/timestep", 0, format_tensor(0, torch.int64))
-
-        observation, reward, terminated, truncated, _ = env.step(action)
-
-        workspace.set("env/env_obs", 1, format_tensor(observation))
-        workspace.set("env/terminated", 1, format_tensor(terminated, torch.bool))
-        workspace.set("env/truncated", 1, format_tensor(truncated, torch.bool))
-        workspace.set("env/done", 1, format_tensor(terminated or truncated, torch.bool))
-        workspace.set("env/reward", 1, format_tensor(reward))
-        workspace.set("env/cumulated_reward", 1, format_tensor(reward))
-        next_action = env.action_space.sample()
-        workspace.set("action", 1, format_tensor(next_action))
-        workspace.set("env/timestep", 1, format_tensor(1, torch.int64))
-
-        rb.put(workspace.get_transitions())
-    return rb
-
-
-def collect_uniform_transitions_2(env_name: str, size: int = 100_000):
-    env = gym.make(env_name)
     # Set up the replay buffer
-    rb = ReplayBuffer(size)
+    rb = ReplayBuffer(buffer_size)
     rb.variables = {}
     rb.variables["env/env_obs"] = torch.empty(
-        [size, 2, env.observation_space.shape[0]], dtype=torch.float32
+        [buffer_size, 2, env.observation_space.shape[0]], dtype=torch.float32
     )
-    rb.variables["env/terminated"] = torch.empty([size, 2], dtype=torch.bool)
-    rb.variables["env/truncated"] = torch.empty([size, 2], dtype=torch.bool)
-    rb.variables["env/done"] = torch.empty([size, 2], dtype=torch.bool)
-    rb.variables["env/reward"] = torch.empty([size, 2], dtype=torch.float32)
-    rb.variables["env/cumulated_reward"] = torch.empty([size, 2], dtype=torch.float32)
-    rb.variables["env/timestep"] = torch.empty([size, 2], dtype=torch.int64)
-    rb.variables["action"] = torch.empty([size, 2, env.action_space.shape[0]])
+    rb.variables["env/terminated"] = torch.empty([buffer_size, 2], dtype=torch.bool)
+    rb.variables["env/truncated"] = torch.empty([buffer_size, 2], dtype=torch.bool)
+    rb.variables["env/done"] = torch.empty([buffer_size, 2], dtype=torch.bool)
+    rb.variables["env/reward"] = torch.empty([buffer_size, 2], dtype=torch.float32)
+    rb.variables["env/cumulated_reward"] = torch.empty([buffer_size, 2], dtype=torch.float32)
+    rb.variables["env/timestep"] = torch.empty([buffer_size, 2], dtype=torch.int64)
+    rb.variables["action"] = torch.empty([buffer_size, 2, env.action_space.shape[0]])
 
-    for i in trange(size):
-        env.reset()
+    for i in trange(buffer_size):
 
-        state_0 = env.observation_space.sample()
-        env.unwrapped.state = state_0
-        action = env.action_space.sample()
+        state_0 = env.uniform_reset()
+        action = env.uniform_action()
         state_1, reward, terminated, truncated, _ = env.step(action)
 
         rb.variables["env/env_obs"][i, 0, :] = torch.tensor(state_0)
@@ -131,7 +129,10 @@ def collect_uniform_transitions_2(env_name: str, size: int = 100_000):
         rb.variables["env/truncated"][i, 1] = torch.tensor(truncated)
 
         rb.variables["env/terminated"][i, 0] = torch.tensor(False)
-        rb.variables["env/terminated"][i, 1] = torch.tensor(truncated)
+        rb.variables["env/terminated"][i, 1] = torch.tensor(terminated)
+
+        rb.variables["env/done"][i, 0] = torch.tensor(False)
+        rb.variables["env/done"][i, 1] = torch.tensor(truncated or terminated)
 
         rb.variables["env/reward"][i, 0] = torch.tensor(0.0)
         rb.variables["env/reward"][i, 1] = torch.tensor(reward)
@@ -139,11 +140,11 @@ def collect_uniform_transitions_2(env_name: str, size: int = 100_000):
         rb.variables["env/cumulated_reward"][i, 0] = torch.tensor(0.0)
         rb.variables["env/cumulated_reward"][i, 1] = torch.tensor(reward)
 
-        rb.variables["env/timestep"][i, 0] = torch.tensor(1)
+        rb.variables["env/timestep"][i, 0] = torch.tensor(0)
         rb.variables["env/timestep"][i, 1] = torch.tensor(1)
 
         rb.variables["action"][i, 0, :] = torch.tensor(action)
-        rb.variables["action"][i, 1, :] = torch.tensor(action)
+        rb.variables["action"][i, 1, :] = torch.zeros_like(torch.tensor(action)) # dummy
 
     rb.is_full = True
 
