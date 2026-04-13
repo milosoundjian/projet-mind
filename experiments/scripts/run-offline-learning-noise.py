bd8de778510b54c528c82eec5a84f5489a17e5ec
Milos Oundjian <milosoundjian@gmail.com>
2026-04-13 22:46:08 +0200

    Update offline running scripts

--- experiments/scripts/run-offline-learning-noise.py
+++ experiments/scripts/run-offline-learning-noise.py
@@ -4,7 +4,7 @@ import argparse
 import torch
 from omegaconf import OmegaConf
 
-from pmind.replay import test_rb_uniform_proportion
+from pmind.replay import test_rb_noise_level
 from pmind.algorithms import TD3
 from pmind.config.loader import load_config
 import bbrl_gymnasium
@@ -17,9 +17,7 @@ def main():
     parser = argparse.ArgumentParser(description="Run offline TD3 experiment")
 
     parser.add_argument("--env", type=str, required=True, help="Environment name")
-    parser.add_argument(
-        "--proportion", type=float, required=True, help="Mix proportion"
-    )
+    parser.add_argument("--noise", type=float, required=True, help="Noise")
     parser.add_argument("--seed", type=int, required=True, help="Random seed")
     parser.add_argument(
         "--exploit_reward", type=int, required=True, help="Exploit reward identifier"
@@ -28,7 +26,7 @@ def main():
     args = parser.parse_args()
 
     ENV_NAME = args.env
-    PROPORTION = args.proportion
+    NOISE = args.noise
     SEED = args.seed
     EXPLOIT_REWARD = args.exploit_reward
 
@@ -44,12 +42,8 @@ def main():
     bbrl_utils.setup()
 
     # ---- load replay buffers ----
-    rb_unif = torch.load(
-        f"/Vrac/21500050/projet-mind/experiments/models/{ENV_NAME}/rb-unif.pt",
-        weights_only=False,
-    )
-    rb_exploit = torch.load(
-        f"/Vrac/21500050/projet-mind/experiments/models/{ENV_NAME}/rb-{EXPLOIT_REWARD}.pt",
+    rb = torch.load(
+        f"/Vrac/21500050/projet-mind/experiments/models/{ENV_NAME}/rb-{EXPLOIT_REWARD}-noise-{NOISE}.pt",
         weights_only=False,
     )
 
@@ -74,19 +68,11 @@ def main():
     cfg_offline.algorithm.target_policy_noise = None
 
     # ---- run ----
-    result = test_rb_uniform_proportion(
-        rb_unif,
-        rb_exploit,
-        BUFFER_SIZE,
-        PROPORTION,
-        TD3,
-        cfg_offline,
-        EXPLOIT_REWARD,
-        SEED,
-    )
+    print(f"seed: {SEED}")
+    result = test_rb_noise_level(rb, NOISE, BUFFER_SIZE, TD3, cfg_offline, SEED)
 
     # ---- save ----
-    output_path = f"/users/Etu0/21500050/results/result-{ENV_NAME}-{EXPLOIT_REWARD}-{PROPORTION}-{SEED}.pt"
+    output_path = f"/users/Etu0/21500050/results/result-{ENV_NAME}-{EXPLOIT_REWARD}-{NOISE}-{SEED}.pt"
     torch.save(result, output_path)
 
     print(f"Saved result to {output_path}")
